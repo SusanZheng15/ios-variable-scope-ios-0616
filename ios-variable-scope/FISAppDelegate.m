a0427030b2f77e68daade8162af0ffdd8637dc06
SusanZheng15 <zheng.susan12@gmail.com>
2016-05-11 10:05:07 -0400

    done

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -15,14 +15,42 @@
 
 @implementation FISAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+-(BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array
+{
+    NSMutableArray *arrayCopy = [NSMutableArray new];
+    
+        for (NSUInteger i = 0 ; i < [array count]; i++)
+        {
+            [arrayCopy addObject:array[i]];
+        }
+    
+      [arrayCopy addObject:string];
+        return arrayCopy;
+}
 
+-(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array
+{
+    NSUInteger countOfUppercaseStringsInArray = 0;
+    
+        for (NSUInteger i = 0 ; i < [array count]; i++)
+        {
+            if ([array[i] isEqualToString:[ array[i] uppercaseString]])
+            {
+                countOfUppercaseStringsInArray++;
+            }
+               NSLog(@"%li", countOfUppercaseStringsInArray);
+        }
+    
+        return countOfUppercaseStringsInArray;
+    }
+
+- (void)removeAllObjectsFromArray:(NSMutableArray *)array
+{
+    [array removeAllObjects];
+}
 @end
